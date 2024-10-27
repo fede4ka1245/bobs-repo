8eda05b1a03ba0e829e55998a1e5fb648fcd1582
fede4ka1245 <bragin.fedor96@gmail.com>
2024-10-27 08:31:49 +0500

    fix

--- server/src/shared/generate_unique_filename.py
+++
@@ -1,7 +0,0 @@
-import uuid
-
-
-def generate_unique_filename(original_filename):
-    _, extension = original_filename.rsplit('.', 1)
-    unique_filename = f"{uuid.uuid4()}.{extension}"
-    return unique_filename
